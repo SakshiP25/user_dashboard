c67e1ed7af7d41511a85e46550268fbee8c27f17
SakshiP25 <167409301+SakshiP25@users.noreply.github.com>
2024-05-14 11:47:36 +0530

    Add files via upload

---
+++ SignInUI.js
@@ -0,0 +1,58 @@
+import "./SignInUI.css";
+
+const SignInUI = () => {
+  return (
+    <div className="signin-ui">
+      <div className="image-1" />
+      <div className="vector-parent">
+        <img className="component-child" alt="" src="/rectangle-21.svg" />
+        <i className="create-now-container">
+          <p className="create-now">Create Now!!!</p>
+          <p className="create-now">{`&`}</p>
+          <p className="create-now">Get Exciting Offers</p>
+        </i>
+      </div>
+      <div className="forgot-your-password">Forgot your password?</div>
+      <b className="sign-in">Sign in</b>
+      <i className="get-the-best-container">
+        <p className="create-now">Get</p>
+        <p className="create-now">The Best Deals!!!</p>
+      </i>
+      <div className="signin-ui-child" />
+      <div className="sign-up">SIGN UP</div>
+      <img className="logo-1-icon" alt="" src="/logo-1@2x.png" />
+      <div className="rectangle-parent">
+        <div className="frame-child" />
+        <div className="frame-item" />
+        <div className="email">Email</div>
+        <div className="password">Password</div>
+        <div className="g-1" />
+        <img className="iconperson" alt="" src="/iconperson.svg" />
+        <div className="iconlock">
+          <div className="iconlock-child" />
+          <div className="iconlock-item" />
+        </div>
+      </div>
+      <div className="vector-group">
+        <img className="frame-inner" alt="" src="/rectangle-28.svg" />
+        <i className="sign-in1">SIGN IN</i>
+      </div>
+      <div className="rectangle-group">
+        <div className="rectangle-div" />
+        <div className="or-continue-with">Or continue with</div>
+        <img
+          className="picsart-24-03-05-12-01-24-944-icon"
+          alt=""
+          src="/picsart-240305-120124944-1@2x.png"
+        />
+        <img
+          className="picsart-24-03-05-11-55-55-633-icon"
+          alt=""
+          src="/picsart-240305-115555633-1@2x.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SignInUI;
